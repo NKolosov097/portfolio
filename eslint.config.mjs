d91666b81b3e0d80c4547ffac60ecf5f1b00ddd0
Kolosov Nikita <N.Kolosov2003@mail.ru>
2025-05-03 08:44:55 +0300

    init: initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,55 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
+import pluginImport from "eslint-plugin-import"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: {
+      import: pluginImport,
+    },
+    rules: {
+      "no-var": "error",
+      "prefer-const": "error",
+      "no-unused-vars": "error",
+      "no-shadow": "error",
+      "no-redeclare": "error",
+      "no-dupe-args": "error",
+      "no-dupe-keys": "error",
+      "no-duplicate-case": "error",
+      "no-dupe-class-members": "error",
+      "no-extra-semi": "error",
+      "no-func-assign": "error",
+      "no-import-assign": "error",
+      "no-invalid-regexp": "error",
+      "no-irregular-whitespace": "error",
+      "no-misleading-character-class": "error",
+      "no-obj-calls": "error",
+      "no-sparse-arrays": "error",
+      "no-template-curly-in-string": "error",
+      "no-unexpected-multiline": "error",
+      "no-unsafe-finally": "error",
+      "no-unsafe-negation": "error",
+      "require-atomic-updates": "error",
+      "use-isnan": "error",
+      "valid-typeof": "error",
+      "import/no-duplicates": "error",
+      "import/no-unresolved": "error",
+      "import/named": "error",
+      "import/namespace": "error",
+      "import/default": "error",
+      "import/export": "error",
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
