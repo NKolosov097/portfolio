e211b33f2b7cd638f54e17bc77320ff906254b79
NKolosov097 <N.Kolosov2003@mail.ru>
2025-05-10 16:20:45 +0300

    fix: deleted unnecessary fetches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,7 @@ const AboutMe = lazy(() => import('@/home-sections/AboutMe/AboutMe'))
 const Resume = lazy(() => import('@/home-sections/Resume/Resume'))
 const Contact = lazy(() => import('@/home-sections/Contact/Contact'))
 
-const getData = async () =>
-  await fetch(`http://localhost:3000/api/hello`)
-    .catch((err) => {
-      console.error(`Error with fetching data for home page: \n${err}`)
-    })
-    .then((res) => res?.json())
-    .catch((err) => {
-      console.error(`Error with transform data for the main page to json format: \n${err}`)
-    })
-
 export default async function HomePage() {
-  console.log(await getData())
-
   return (
     <>
       <Home />
