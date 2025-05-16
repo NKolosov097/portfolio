a05591e91d50da50407edba1378489128cd85c77
NKolosov097 <N.Kolosov2003@mail.ru>
2025-05-16 14:37:39 +0300

    fix(deploy): updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -135,7 +135,7 @@ server {
     limit_req zone=mylimit burst=20 nodelay;
 
     location / {
-        proxy_pass http://localhost:3000;
+        proxy_pass http://127.0.0.1:3000;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
@@ -143,6 +143,14 @@ server {
         proxy_cache_bypass \$http_upgrade;
         proxy_buffering off;
         proxy_set_header X-Accel-Buffering no;
+    }
+
+    location /hooks/ {
+        proxy_pass http://127.0.0.1:9000/hooks/;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
     }
 }
 EOL
