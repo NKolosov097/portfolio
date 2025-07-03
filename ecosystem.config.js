f33facd51ba133670383921ddcf34a519f694ab5
NKolosov097 <N.Kolosov2003@mail.ru>
2025-07-03 22:41:20 +0300

    dix(deploy): updated files for vps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'portfolio',
-      script: '.next/standalone/server.js',
-      cwd: '/var/www/portfolio',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3000,
-      },
+      script: 'pnpm install && pnpm build && pnpm start',
+      time: true,
+      log_date_format: 'DD.MM.YYYY HH:mm:ss',
     },
   ],
 }
