ee1b7157b3a19bdd0453777f69bac44a987b8fd2
Kolosov Nikita <n.kolosov2003@mail.ru>
2026-08-01 10:14:50 +0300

    fix: updated styles of advantage cards

--- src/home-sections/AboutMe/components/Advantage/Advantage.tsx
+++ src/home-sections/AboutMe/components/Advantage/Advantage.tsx
@@ -2,9 +2,9 @@ import styles from '@/home-sections/AboutMe/AboutMe.module.css'
 
 import { IAdvantage } from '@/home-sections/AboutMe/types/aboutMe.type'
 
-export const Advantage = ({ title, description }: IAdvantage) => {
+export const Advantage = ({ id, title, description }: IAdvantage) => {
   return (
-    <li className={styles.advantage}>
+    <li className={styles.advantage} data-key={id}>
       <h3 className={styles.advantageHeader}>{title}</h3>
       <p className={styles.advantageDescription}>{description}</p>
     </li>
